07d53e1c389710c23ab656b2649755c5b8aa9749
danillobarbosa <danillobarbosa@gmail.com>
2017-10-23 21:20:27 -0200

    Rota para pagina de sobre
    Adicionado modulo de rotas e criado component para página de sobre

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,19 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Routes } from "@angular/router";
+import { RouterModule } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { NavbarModule } from "./navbar/navbar.module";
+import { HomeComponent } from "./home/home.component";
+import { SobreComponent } from './sobre/sobre.component';
+
+const routes: Routes = [{ path: "", component: HomeComponent }, {path: 'sobre', component: SobreComponent}];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, SobreComponent],
   imports: [
     BrowserModule,
     NavbarModule,
+    RouterModule.forRoot(routes),
     NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
