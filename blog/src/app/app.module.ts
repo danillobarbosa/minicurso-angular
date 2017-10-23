07e88a307224459b01a8680c46220c6e8660ee4d
danillobarbosa <danillobarbosa@gmail.com>
2017-10-23 20:42:50 -0200

    Adicionado bootstrap e componente de navbar

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NavbarModule } from "./navbar/navbar.module";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavbarModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
