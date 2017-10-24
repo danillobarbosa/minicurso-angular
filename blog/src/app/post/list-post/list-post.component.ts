6f4890d1aec49132339ab88007e5deba0fc5ada0
danillobarbosa <danillobarbosa@gmail.com>
2017-10-24 03:45:51 -0200

    NgOndestroy

--- blog/src/app/post/list-post/list-post.component.ts
+++ blog/src/app/post/list-post/list-post.component.ts
@@ -1,19 +1,28 @@
 import { PostService } from "./../post.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
+import { Subscription } from "rxjs";
 @Component({
   selector: "app-list-post",
   templateUrl: "./list-post.component.html",
   styleUrls: ["./list-post.component.css"]
 })
-export class ListPostComponent implements OnInit {
+export class ListPostComponent implements OnInit, OnDestroy {
   posts: Array<any> = [];
 
+  subscription: Subscription;
+
   constructor(private postService: PostService) {
-    this.postService.getPosts().subscribe(res => {
+    this.subscription = this.postService.getPosts().subscribe(res => {
       this.posts = res.news;
     });
   }
 
   ngOnInit() {}
+
+  ngOnDestroy(){
+
+    this.subscription.unsubscribe();
+
+  }
 }
